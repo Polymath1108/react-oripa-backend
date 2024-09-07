8bb99fe1598f434781ce7726a213586e155129c5
Oliver96118 <178637468+Oliver96118@users.noreply.github.com>
2024-09-08 02:14:01 +0800

    feat:add vercel.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 
 // config.js
 const config = {
-    server: {
-      PORT: 5000,
-      HOST: 'localhost',
-    },
-    database: {
-      HOST: '127.0.0.1',
-      USER: 'root',
-      PASSWORD: '',
-      NAME: 'Oripa_DB',
-    },
-    api: {
-      VERSION: 'v1',
-      KEY: 'api_key',
-    },
+    // server: {
+    //   PORT: 5000,
+    //   HOST: 'localhost',
+    // },
+    // database: {
+    //   HOST: '127.0.0.1',
+    //   USER: 'root',
+    //   PASSWORD: '',
+    //   NAME: 'Oripa_DB',
+    // },
+    // api: {
+    //   VERSION: 'v1',
+    //   KEY: 'api_key',
+    // },
     admin_authority: {
       admin: "administrators", //authority for managing administrator
       users: "users", //authority for managing users
